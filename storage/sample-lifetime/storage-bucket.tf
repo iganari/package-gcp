40cd6e8dab246216ec257532a3f16a2d8dfe009c
iganari <cigaguri@gmail.com>
2019-10-24 09:07:47 +0900

    all commentout

--- storage/sample-lifetime/storage-bucket.tf
+++ storage/sample-lifetime/storage-bucket.tf
@@ -1,6 +1,14 @@
-resource "google_storage_bucket" "gsb" {
-  name     = "image-store-bucket"
-  location = "asia-northeast1"
-
-
-}
+# resource "google_storage_bucket" "image-store" {
+#   name     = "image-store-bucket-1024-02"
+#   location = "asia-northeast1"
+# 
+#   project = "${terraform.workspace}"
+#   storage_class = "REGIONAL"
+#   force_destroy = false
+# 
+# 
+#   # website {
+#   #   main_page_suffix = "index.html"
+#   #   not_found_page   = "404.html"
+#   # }
+# }
