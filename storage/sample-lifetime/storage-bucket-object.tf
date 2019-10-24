893168fc491db1a9cdbcf85e144cfef14d918346
iganari <cigaguri@gmail.com>
2019-10-25 08:52:23 +0900

    add object access control

--- storage/sample-lifetime/storage-bucket-object.tf
+++ storage/sample-lifetime/storage-bucket-object.tf
@@ -1,5 +1,26 @@
 resource "google_storage_bucket_object" "gsbo-pic-01" {
     name = "animal_chara_computer_penguin.png"
     source = "images/animal_chara_computer_penguin.png"
-    bucket = "${google_storage_bucket.gsb.name}"
+    bucket = "${google_storage_bucket.gsb-01.name}"
+}
+
+
+resource "google_storage_bucket_object" "gsbo-pic-02" {
+    name = "animal_chara_smartphone_penguin.png"
+    source = "images/animal_chara_smartphone_penguin.png"
+    bucket = "${google_storage_bucket.gsb-02.name}"
+}
+
+
+resource "google_storage_bucket_object" "gsbo-pic-03" {
+    name = "animal_chara_smartphone_penguin.png"
+    source = "images/animal_chara_smartphone_penguin.png"
+    bucket = "${google_storage_bucket.gsb-03.name}"
+}
+
+resource "google_storage_object_access_control" "gsoac-03" {
+    object = "${google_storage_bucket_object.gsbo-pic-03.output_name}"
+    bucket = "${google_storage_bucket.gsb-03.name}"
+    role = "READER"
+    entity = "allUsers"
 }
--- storage/sample-lifetime/storage-bucket.tf
+++ storage/sample-lifetime/storage-bucket.tf
@@ -1,11 +1,5 @@
-# resource "random_id" "bucket-suffix" {
-#     byte_length = 4
-# 
-# 
-# }
 
-
-resource "random_string" "bucket-suffix" {
+resource "random_string" "bucket-suffix-01" {
     length = 8
     upper = false
     lower = true
@@ -17,16 +11,8 @@ resource "random_string" "bucket-suffix" {
 #     value = "${random_string.bucket-suffix}"
 # }
 
-resource "random_string" "bucket-suffix-02" {
-    length = 8
-    upper = false
-    lower = true
-    number = false
-    special = false
-}
-
-resource "google_storage_bucket" "gsb" {
-  name     = "image-store-bucket-${random_string.bucket-suffix.id}"
+resource "google_storage_bucket" "gsb-01" {
+  name     = "bucket-test-public-01-${random_string.bucket-suffix-01.id}"
   location = "asia-northeast1"
 
   project = "${terraform.workspace}"
@@ -40,8 +26,24 @@ resource "google_storage_bucket" "gsb" {
   # }
 }
 
+# resource "google_storage_bucket_access_control" "gsbac_public_rule-01" {
+#   bucket = "${google_storage_bucket.gsb-01.name}"
+#   role = "READER"
+#   entity = "allUsers"
+# } 
+
+
+# ================================================= #
+
+resource "random_string" "bucket-suffix-02" {
+    length = 8
+    upper = false
+    lower = true
+    number = false
+    special = false
+}
 resource "google_storage_bucket" "gsb-02" {
-  name     = "image-store-bucket-${random_string.bucket-suffix-02.id}"
+  name     = "bucket-test-public-02-${random_string.bucket-suffix-02.id}"
   location = "asia-northeast1"
 
   project = "${terraform.workspace}"
@@ -54,3 +56,29 @@ resource "google_storage_bucket_access_control" "gsbac_public_rule" {
   role = "READER"
   entity = "allUsers"
 } 
+
+# ================================================= #
+
+resource "random_string" "bucket-suffix-03" {
+    length = 8
+    upper = false
+    lower = true
+    number = false
+    special = false
+}
+
+resource "google_storage_bucket" "gsb-03" {
+  name     = "bucket-test-public-03-${random_string.bucket-suffix-03.id}"
+  location = "asia-northeast1"
+
+  project = "${terraform.workspace}"
+  storage_class = "REGIONAL"
+  force_destroy = false
+}
+
+# resource "google_storage_bucket_access_control" "gsbac_public_rule-03" {
+#   bucket = "${google_storage_bucket.gsb-03.name}"
+#   role = "READER"
+#   entity = "allAuthenticatedUsers"
+#   # entity = "cigaguri@gmail.com"
+# } 
