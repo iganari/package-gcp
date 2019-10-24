8d0eb30886604a89f1cc59433e3816ea5035c5c5
iganari <cigaguri@gmail.com>
2019-10-25 06:52:25 +0900

    add random string for GCS

--- storage/sample-lifetime/storage-bucket.tf
+++ storage/sample-lifetime/storage-bucket.tf
@@ -1,14 +1,35 @@
-# resource "google_storage_bucket" "image-store" {
-#   name     = "image-store-bucket-1024-02"
-#   location = "asia-northeast1"
-# 
-#   project = "${terraform.workspace}"
-#   storage_class = "REGIONAL"
-#   force_destroy = false
+# resource "random_id" "bucket-suffix" {
+#     byte_length = 4
 # 
 # 
-#   # website {
-#   #   main_page_suffix = "index.html"
-#   #   not_found_page   = "404.html"
-#   # }
 # }
+
+
+resource "random_string" "bucket-suffix" {
+    length = 8
+    upper = false
+    lower = true
+    number = false
+    special = false
+}
+
+output "output-random-string" {
+    value = "${random_string.bucket-suffix}"
+}
+
+
+
+resource "google_storage_bucket" "gsb" {
+  name     = "image-store-bucket-${random_string.bucket-suffix.id}"
+  location = "asia-northeast1"
+
+  project = "${terraform.workspace}"
+  storage_class = "REGIONAL"
+  force_destroy = false
+
+
+  # website {
+  #   main_page_suffix = "index.html"
+  #   not_found_page   = "404.html"
+  # }
+}
