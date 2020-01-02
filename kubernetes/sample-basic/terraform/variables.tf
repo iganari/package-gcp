788b8e284fbdaeafe99d3c8a3ab21faa1d596437
iganari <cigaguri@gmail.com>
2020-01-02 12:16:00 +0900

    modify deprecated var

--- kubernetes/sample-basic/terraform/variables.tf
+++ kubernetes/sample-basic/terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "network" {
-  type = "map"
+  # type = "map"
   default = {
     nw-name                    = "iganari-nw"
     nw-auto_create_subnetworks = "false"
@@ -10,7 +10,7 @@ variable "network" {
 }
 
 variable "cluster" {
-  type = "map"
+  # type = "map"
   default = {
     pr-name     = "iganari-k8s-primary"
     master-name = "k8s-admin"
@@ -19,9 +19,9 @@ variable "cluster" {
 }
 
 variable "node-pool" {
-  type = "map"
+  # type = "map"
   default = {
-    np-name         = "iganari-k8s-node"
+    np-name = "iganari-k8s-node"
     # master-name     = "k8s-admin"
     # master-pass     = "hogehogefugafuga"
     np-machine-type = "n1-standard-1"
