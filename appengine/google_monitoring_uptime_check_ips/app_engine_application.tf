b9a3a840d1eff78a16ab89b45e3cd704cc574404
iganari <cigaguri@gmail.com>
2020-05-17 09:33:13 +0900

    add variable file

--- appengine/google_monitoring_uptime_check_ips/app_engine_application.tf
+++ appengine/google_monitoring_uptime_check_ips/app_engine_application.tf
@@ -1,4 +1,4 @@
 resource "google_app_engine_application" "pkg-gcp" {
   depends_on  = [google_project_service.pkg-gcp_appengine]
-  location_id = lookup(var.common, "region")
+  location_id = var.region
 }
